821fe6970647ec2729de06f6d25d2e6ad7a59165
shiv loka <shiv.loka@appdynamics.com>
2016-06-22 18:26:07 -0700

    required instructions and support endpoint for slack added

--- src/main/java/com/appdhack/sup/resources/SupResource.java
+++ src/main/java/com/appdhack/sup/resources/SupResource.java
@@ -12,4 +12,20 @@ public class SupResource {
     public Response ping() {
         return Response.ok("hi").build();
     }
+
+    @Path("/howto")
+    @GET
+    public Response howTo() {
+        String instructions = "This application integrated with Slack will help schedule and manage your stand ups.";
+
+        return Response.ok(instructions).build();
+    }
+
+    @Path("/support")
+    @GET
+    public Response support() {
+        String support = "For support please contact Shashank Devan or John Lee or Shiv Loka.";
+
+        return Response.ok(support).build();
+    }
 }
